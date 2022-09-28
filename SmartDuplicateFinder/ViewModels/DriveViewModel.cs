3b4bf468c7ceef1a4a9ef9dc673e3c1d6378da1c
tHolloway <tHolloway@DEV011>
2022-09-28 13:37:19 -0500

    Add drive list

---
+++ SmartDuplicateFinder/ViewModels/DriveViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SmartDuplicateFinder.ViewModels;
+
+public class DriveViewModel
+{
+	public DriveViewModel(DriveInfo driveInfo)
+	{
+		DriveInfo = driveInfo;
+		Name = $"{DriveInfo.VolumeLabel} ({DriveInfo.Name[..^1]})";
+	}
+
+	public string Name { get; protected set; }
+	public DriveInfo DriveInfo { get; private set; }
+}
